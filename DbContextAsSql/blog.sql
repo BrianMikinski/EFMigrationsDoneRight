c7a630f8e1e37a49919f74859b444a4a035463ce
Brian Mikinski <brian.mikinski@gmail.com>
2023-11-10 00:15:23 -0600

    add blog sql

---
+++ DbContextAsSql/blog.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [Posts] (
+    [Id] uniqueidentifier NOT NULL,
+    [Title] nvarchar(50) NULL,
+    [Content] nvarchar(1000) NULL,
+    CONSTRAINT [PK_Posts] PRIMARY KEY ([Id])
+);
+GO
+
+
